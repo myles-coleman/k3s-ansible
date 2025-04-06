a8c7f43f381074277809d52499a903bb6c51ed4f
myles-coleman <mylescoleman05@gmail.com>
2025-04-06 23:46:02 +0000

    final changes to docker file

--- bitwarden-docker/entrypoint.sh
+++ bitwarden-docker/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
+
+BW_CLIENTID=$(echo -n "$BW_CLIENTID" | tr -d '\n\r')
+BW_CLIENTSECRET=$(echo -n "$BW_CLIENTSECRET" | tr -d '\n\r')
+BW_PASSWORD=$(echo -n "$BW_PASSWORD" | tr -d '\n\r')
+
 bw config server ${BW_HOST}
 bw login --apikey
-export BW_SESSION=$(bw unlock ${BW_PASSWORD} --raw)
+export BW_SESSION=$(bw unlock "${BW_PASSWORD}" --raw)
 bw unlock --check
 echo 'Running `bw server` on port 8087'
 bw serve --hostname 0.0.0.0
